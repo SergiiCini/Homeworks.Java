4ac570483e160c017d440a5a230b1a10c7a34854
Serhii Romaniuk <romaniuk87@gmail.com>
2021-01-06 01:32:13 +0200

    --initial

--- 8/src/main/java/app/FamilyService.java
+++ 8/src/main/java/app/FamilyService.java
@@ -1,5 +1,8 @@
 package app;
 
+import jdk.vm.ci.meta.Local;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +17,8 @@ public class FamilyService {
         return familyDao.getAllFamilies();
     }
 
-    public <T> void printData(List<T> data){
-        for(T item: data) {
+    public <T> void printData(List<T> data) {
+        for (T item : data) {
             System.out.println(item);
         }
     }
@@ -31,9 +34,9 @@ public class FamilyService {
 
     public List<Family> getFamiliesBiggerThan(int numberOfFamilyMembers) {
         List<Family> filteredFamilies = new ArrayList<>();
-        List<Family> allFamilies = familyDao.getAllFamilies();
-        for(Family family : allFamilies) {
-            if(family.countFamily() > numberOfFamilyMembers){
+        List<Family> allFamilies = getAllFamilies();
+        for (Family family : allFamilies) {
+            if (family.countFamily() > numberOfFamilyMembers) {
                 assert false;
                 filteredFamilies.add(family);
             }
@@ -44,9 +47,9 @@ public class FamilyService {
 
     public List<Family> getFamiliesLessThan(int numberOfFamilyMembers) {
         List<Family> filteredFamilies = new ArrayList<>();
-        List<Family> allFamilies = familyDao.getAllFamilies();
-        for(Family family : allFamilies) {
-            if(family.countFamily() < numberOfFamilyMembers){
+        List<Family> allFamilies = getAllFamilies();
+        for (Family family : allFamilies) {
+            if (family.countFamily() < numberOfFamilyMembers) {
                 assert false;
                 filteredFamilies.add(family);
             }
@@ -54,6 +57,59 @@ public class FamilyService {
         printData(allFamilies);
         return filteredFamilies;
     }
+
+    public int countFamiliesWithMemberNumber(int numberOfFamilyMembers) {
+        List<Family> filteredFamilies = new ArrayList<>();
+        List<Family> allFamilies = getAllFamilies();
+        for (Family family : allFamilies) {
+            if (family.countFamily() == numberOfFamilyMembers) {
+                assert false;
+                filteredFamilies.add(family);
+            }
+        }
+        System.out.println(filteredFamilies.size());
+        return filteredFamilies.size();
+    }
+
+    public Family createNewFamily(Human mother, Human father) {
+        Family family = new Family(mother, father);
+        familyDao.saveFamily(family);
+        return family;
+    }
+
+    public boolean deleteFamilyByIndex(int index) {
+        return familyDao.deleteFamilyByIndex(index);
+    }
+
+    public Family bornChild(Family family, String sonsName, String daughterName) {
+        //born child
+        Human father = family.getFather();
+        String surname = father.getSurname();
+        int barthYear = LocalDate.now().getYear();
+        int childGenderProbability = (int) Math.floor(Math.random());
+        String childGender = (childGenderProbability == 1) ? "Boy" : "Girl";
+        switch (childGender) {
+            case "Boy":
+                Man son = new Man(sonsName, surname, barthYear, 0);
+                family.addChild(son);
+                familyDao.saveFamily(family);
+                return family;
+            case "Girl":
+                Woman daughter = new Woman(daughterName, surname, barthYear, 0);
+                family.addChild(daughter);
+                familyDao.saveFamily(family);
+                return family;
+            default:
+                return null;
+        }
+    }
+
+    public Family adoptChild(Family family, Human child){
+        family.addChild(child);
+        familyDao.saveFamily(family);
+        return family;
+    }
+
 
 
 }
